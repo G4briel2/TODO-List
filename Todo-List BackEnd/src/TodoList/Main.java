65b0e7adfdf803b6decccd96a5049d67ed53383d
Gabriel de Almeida Martins <gabriel_241@outlook.com.br>
2023-09-05 20:44:55 -0300

    BackEnd-Java(T004-Refactor): refactor - Refatoração da classe Main

--- Todo-List BackEnd/src/TodoList/Main.java
+++ Todo-List BackEnd/src/TodoList/Main.java
@@ -1,66 +1,97 @@
 package TodoList;
 
-import TodoList.controller.FeatTarefas;
+import TodoList.controller.FileManagerController;
+import TodoList.controller.ListaDeTarefasController;
+import TodoList.model.Tarefa;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Set;
+
+import static TodoList.controller.validators.IntValidator.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner tec = new Scanner(System.in);
-        FeatTarefas t1 = new FeatTarefas();
+        int opc;
+        Scanner input = new Scanner(System.in);
 
-        t1.carregarTarefas();
+        ListaDeTarefasController controladorTarefas = new ListaDeTarefasController();
+        FileManagerController arquivamentoTarefas = new FileManagerController();
 
-        System.out.println("#TODO-LIST#");
-        int opcao = 0;
-        boolean repeat = true;
+        ArrayList<Tarefa> tarefas = controladorTarefas.getListaDeTarefas();
+        boolean temTarefas = controladorTarefas.temCadastrosNaLista(tarefas);
 
-        do{
-            System.out.println("Escolha uma opção:\n1 - Listar tarefas\n2 - Criar tarefas\n3 - Editar tarefas\n4 - Excluir tarefas\n5 - Sair");
-            if (tec.hasNextInt()){
-                opcao = tec.nextInt();
-                if (opcao >=1 && opcao <=5){
-                    if (opcao == 1){
-                        t1.listarTarefasFiltradas();
-                    }
+        Set<String> categorias = controladorTarefas.getCategoriasTarefa();
 
-                    else if (opcao == 2){
-                        t1.criarTarefas();
-                    }
+        do {
+            System.out.println("""
+                    #To Do List#
+                    Selecione uma opção
+                    1 - Listar tarefas
+                    2 - Cadastrar tarefa
+                    3 - Editar tarefa
+                    4 - Excluir tarefa
+                    0 - Sair""");
 
-                    else if (opcao == 3){
-                        System.out.println("vai editar uma tarefa");
-                    }
+            opc = forcarInputInteiro();
 
-                    else if (opcao == 4){
-                        t1.excluirTarefa();
-                    }
+            switch (opc) {
+                case 1 -> {
+                    if (temTarefas) {
+                        System.out.println("#Listagem#\n1 - Categoria\n2 - Prioridade\n3 - Status\n4 - Nenhum\nSelecione um filtro");
+                        int filtroSelecionado = intMaiorQue(0, forcarInputInteiro());
 
-                    else {
-                        try {
-                            t1.salvarTarefas();
-                        } catch (IOException e) {
-                            System.out.println("Erro ao salvar tarefas");
-                            try {
-                                Thread.sleep(3000);
-                            } catch (InterruptedException ex) {
-                                throw new RuntimeException(ex);
+                        switch (filtroSelecionado) {
+                            case 1 -> {
+                                System.out.println(controladorTarefas.listarCategorias() + "\nSelecione uma categoria");
+                                int categoriaEscolhida = intMaiorQueEMenorQue(0, forcarInputInteiro(), categorias.size()+1);
+
+                                System.out.println(controladorTarefas.listarTarefasPorCategoria(categoriaEscolhida));
                             }
+                            case 2 -> {
+                                System.out.println("1 - Muito baixo\n2 - Baixo\n3 - Médio\n4 - Alto\n5 - Muito alto\nSelecione uma prioridade");
+                                int prioridadeEscolhida = intMaiorQueEMenorQue(0, forcarInputInteiro(), 6);
+
+                                System.out.println(controladorTarefas.listarTarefasPorPrioridade(prioridadeEscolhida));
+                            }
+                            case 3 -> {
+                                System.out.println("1 - To do\n2 - Doing\n3 - Done\nSelecione um status de tarefa");
+                                int statusEscolhido = intMaiorQueEMenorQue(0, forcarInputInteiro(), 4);
+
+                                System.out.println(controladorTarefas.listarTarefasPorStatus(statusEscolhido));
+                            }
+                            case 4 -> System.out.println(controladorTarefas.listarTarefasSemFiltro());
+                            default -> System.out.println("Opção inválida");
                         }
-                        repeat = false;
+                    } else {
+                        System.out.println("Nenhuma tarefa cadastrada");
                     }
                 }
+                case 2 -> controladorTarefas.cadastrarTarefa(controladorTarefas.criarTarefa());
+                case 3 -> {
+                    System.out.println(controladorTarefas.listarTarefasSemFiltro() + "Que tarefa deseja editar ?");
+                    int tarefaSelecionada = intMaiorQueEMenorQue(0, forcarInputInteiro(), tarefas.size()+1)-1;
 
-                else {
-                    System.out.println("Opção inválida");
+                    controladorTarefas.editarTarefa(controladorTarefas.criarTarefa(), tarefaSelecionada);
                 }
-            }
+                case 4 -> {
+                    if (temTarefas) {
+                        System.out.println(controladorTarefas.listarTarefasSemFiltro() + "0 - Para cancelar\n#Excluir tarefa#\nQue tarefa deseja excluir?");
+                        int indice = intMaiorQueEMenorQue(0, forcarInputInteiro(), tarefas.size()+1) -1;
 
-            else{
-                tec.nextLine();
-                System.out.println("Opção inválida");
+                        if (indice != 0) {
+                            controladorTarefas.excluirTarefa(indice);
+                        }
+                    } else {
+                        System.out.println("Nenhuma tarefa cadastrada");
+                    }
+                }
+                case 0 -> {
+                    System.out.println("Saindo...");
+                    arquivamentoTarefas.salvarTarefas(tarefas);
+                }
+                default -> System.out.println("Opção inválida");
             }
-        }while (repeat);
+        }while (opc != 0);
     }
 }
